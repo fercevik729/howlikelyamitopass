6f8c362d87eaa8a1a5ca920ad28995fdd34c7e97
Daniel Chang <dawichan@ucsc.edu>
2024-01-20 15:33:44 -0800

    Added onClick events to buttons

--- src/app/evaluate/page.tsx
+++ src/app/evaluate/page.tsx
@@ -1,25 +1,43 @@
+'use client';
 import SkillsComponent from "@/components/skills"
+import React from "react";
 
 export default function Evaluate() {
 
+    const [state, setState] = React.useState('Click an option');
+    
+    const handleChoose = (option: string) => {
+        setState(option);
+    };
+    const handleSkip = () => {
+        setState('skip');
+    };
+    const handleBack = () => {
+        setState('back');
+    };
+
     /*
-        Sample Skills:
-        
+        Sample Sections
+
         <SkillsComponent
             title='CSE 130 - Kerry Veenstra'
             question='What grade did you get in CSE 13S?'
             options={['A', 'B', 'C', 'D', 'F']}
             circles={true}
+            handleChoose={handleChoose}
+            handleBack={handleBack}
+            handleSkip={handleSkip}
         />
-
+        
         <SkillsComponent
             title='CSE 130 - Kerry Veenstra'
             question='Across all your classes, how many hours of
             discussion sections/office hours do you attend each week?'
             options={['None', '1-2', '3-5', '6-8', '9+']}
             circles={false}
+            handleChoose={handleChoose}
+            handleBack={handleBack}
+            handleSkip={handleSkip}
         />
-
     */
-
 }
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { Button, ButtonBase } from "@mui/material";
 
 export default function SkillsComponent(props: {
     title: string
     question: string
     options: string[]
     circles: boolean
+    handleChoose: Function
+    handleSkip: Function
+    handleBack: Function
 }) {
+
     return (
         <div className="w-full flex flex-col items-center">
             <p className="text-2xl mb-8 text-center">{props.title}</p>
@@ -17,21 +20,25 @@ export default function SkillsComponent(props: {
                         {props.options.map(option => {
                             if (props.circles) {
                                 return (
-                                    <div className="transition-all duration-150 mx-4 w-12 h-12 rounded-full border-2 border-white hover:bg-white hover:text-black flex items-center justify-center">
+                                    <button onClick={() => props.handleChoose(option)} key={`${props.question} -> ${option}`} className="transition-all duration-150 mx-4 w-12 h-12 rounded-full border-2 border-white hover:bg-white hover:text-black flex items-center justify-center">
                                         {option}
-                                    </div>
+                                    </button>
                                 )
                             }
                             return (
-                                <div className="transition-all duration-150 mx-4 w-20 h-12 rounded-md border-2 border-white hover:bg-white hover:text-black flex items-center justify-center">
+                                <button onClick={() => props.handleChoose(option)} key={`${props.question} -> ${option}`} className="transition-all duration-150 mx-4 w-20 h-12 rounded-md border-2 border-white hover:bg-white hover:text-black flex items-center justify-center">
                                     {option}
-                                </div>
+                                </button>
                             )
                         })}
                 </div>
             </div>
-            <p className="text-sm text-gray-300 hover:text-gray-100">Skip</p>
-            <p className="text-sm text-gray-300 hover:text-gray-100">Back</p>
+            <button>
+            <p onClick={() => props.handleSkip()} className="text-sm text-gray-300 hover:text-gray-100">Skip</p>
+            </button>
+            <button>
+            <p onClick={() => props.handleBack()} className="text-sm text-gray-300 hover:text-gray-100">Back</p>
+            </button>
         </div>
     );
 }
