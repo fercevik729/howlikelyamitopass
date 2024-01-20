da10ede0b3cb1bf810044edf902a8d47ea909d08
Furkan Ercevik <ercevik.furkan@gmail.com>
2024-01-20 13:00:15 -0800

    Added githooks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Inter, Raleway, Roboto } from "next/font/google";
+import { Raleway, Roboto } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-});
+// const inter = Inter({
+//   subsets: ["latin"],
+//   variable: "--font-inter",
+// });
 
 const raleway = Raleway({
   subsets: ["latin"],
