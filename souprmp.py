3581f1e459e704fb5cd70a29f88386d40c4c92bf
denny <denny@Kitsune>
2024-01-20 10:44:47 -0800

    new tools but isnt good

--- souprmp.py
+++ souprmp.py
@@ -1,4 +1,4 @@
-import requests
+'''import requests
 from bs4 import BeautifulSoup
 
 # Professor URL (Long)
@@ -29,3 +29,30 @@ for i in feedback:
     num = i.find("div", {"class": "FeedbackItem__FeedbackNumber-uof32n-1 kkESWs"}).get_text(strip=True)
     desc = i.find("div", {"class": "FeedbackItem__FeedbackDescription-uof32n-2 hddnCs"}).get_text(strip=True)
     # print(f"{desc}: {num}")
+'''
+
+import requests
+from bs4 import BeautifulSoup
+import ratemyprofessor
+from RateMyProfessorPyAPI import RMPClass
+
+aapi = RMPClass.RateMyProfAPI(schoolId = 1078, teacher = "Kerry Veenstra")
+aapi.retrieveRMPInfo()
+aapi.getTags()
+professor = ratemyprofessor.get_professor_by_school_and_name(ratemyprofessor.get_school_by_name("University of California Santa Cruz"), "Kerry Veenstra")
+if professor is not None:
+    url = professor.__dict__.get('url')
+    if url is not None:
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        tags = soup.select('.Tag-bs9vf4-0')
+        for tag in tags:
+            print(tag.text)
+    else:
+        print("The URL attribute of the professor object is None.")
+        print("%s works in the %s Department of %s." % (professor.name, professor.department, professor.school.name))
+        print("Rating: %s / 5.0" % professor.rating)
+        print("Difficulty: %s / 5.0" % professor.difficulty)
+        print("Total Ratings: %s" % professor.num_ratings)
+        if professor.would_take_again is not None:
+            print(("Would Take Again: %s" % round(professor.would_take_again, 1)) + '%')
