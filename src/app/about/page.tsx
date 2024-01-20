a3234bcbf610f4e7c2fdb68beed9459eb3f51826
Furkan Ercevik <ercevik.furkan@gmail.com>
2024-01-20 13:56:24 -0800

    Fix merge conflicts

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,17 +1,13 @@
-<<<<<<< HEAD
 export default function About() {
-	return (
-	<>
-		<main>
-			<section id={"about_us"} className={'flex py36 items-center'} >
-				<div className={"flex flex-col gap-[52px] text-white"}>
-					<h1 className= {"text-6x1 font-bold"}> About</h1>
-				</div>
-			</section>
-		</main>
-	</>
-	);
+  return (
+    <>
+      <main>
+        <section id={"about_us"} className={"flex py36 items-center"}>
+          <div className={"flex flex-col gap-[52px] text-white"}>
+            <h1 className={"text-6x1 font-bold"}> About</h1>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
-=======
-export default function About() {}
->>>>>>> refs/remotes/origin/main
