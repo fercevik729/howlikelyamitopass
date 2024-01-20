b9082ab316ae5b7097f6d4833f5bd43c899cae30
Jose Chavez <josmchavz@gmail.com>
2024-01-20 15:02:42 -0800

    lint

--- src/components/ProfessorChoice.tsx
+++ src/components/ProfessorChoice.tsx
@@ -1,18 +1,18 @@
 "use client";
-import {Course, CourseOffer} from "@/types/model";
-import {Button, Typography} from "@mui/material";
-import {useState} from "react";
+import { Course, CourseOffer } from "@/types/model";
+import { Button, Typography } from "@mui/material";
+import { useState } from "react";
 
 interface ProfessorChoiceProps {
   courses: Course[];
 }
 
-export default function ProfessorChoice({courses}: ProfessorChoiceProps) {
+export default function ProfessorChoice({ courses }: ProfessorChoiceProps) {
   // Seperate the courses into groups by quarter year
-  const coursesByQuarterYear: {[key: string]: Course[]} = {};
-  const [quarterSelected, setQuarterSelected] = useState<string>('');
-  const [courseSelected, setCourseSelected] = useState<Course>()
-  const [professorSelected, setProfessorSelected] = useState<string>('')
+  const coursesByQuarterYear: { [key: string]: Course[] } = {};
+  const [quarterSelected, setQuarterSelected] = useState<string>("");
+  const [courseSelected, setCourseSelected] = useState<Course>();
+  const [professorSelected, setProfessorSelected] = useState<string>("");
 
   courses.forEach((course: Course) => {
     const quartersOffered: CourseOffer[] = course.offered;
@@ -25,66 +25,103 @@ export default function ProfessorChoice({courses}: ProfessorChoiceProps) {
     });
   });
 
-    return (
-        <div>
-            <h1>ProfessorChoice</h1>
-          <div className={"flex gap-6"}>
-            <ul className={"flex flex-col gap-3"}>
-              {Object.keys(coursesByQuarterYear).map((quarterYear: string) => {
-                return (
-                  <li key={quarterYear}>
-                    <Button variant={"contained"} style={{
-                      backgroundColor: quarterSelected === quarterYear ? 'green' : 'white',
-                      color: quarterSelected === quarterYear ? 'white' : 'black',
-                    }} onClick={() => setQuarterSelected(quarterYear)}>
-                      {quarterYear}
-                    </Button>
-                  </li>
-                )
-              })}
+  return (
+    <div className={"max-w-[1000px] mt-16 mx-auto px-[25px]"}>
+      <div className={"flex flex-col gap-6"}>
+        <ul className={"flex gap-3"}>
+          {Object.keys(coursesByQuarterYear).map((quarterYear: string) => {
+            return (
+              <li key={quarterYear}>
+                <Button
+                  variant={"outlined"}
+                  style={{
+                    backgroundColor:
+                      quarterSelected === quarterYear ? "white" : "transparent",
+                    color: quarterSelected === quarterYear ? "black" : "white",
+                    border: "1px solid white",
+                    borderRadius: "10px",
+                  }}
+                  onClick={() => setQuarterSelected(quarterYear)}
+                >
+                  {quarterYear}
+                </Button>
+              </li>
+            );
+          })}
+        </ul>
+        <div className={"flex gap-16"}>
+          <div>
+            <Typography variant={"h6"}>Classes</Typography>
+            <ul className={"flex flex-col gap-3 h-[60vh] overflow-scroll px-3"}>
+              {quarterSelected &&
+                coursesByQuarterYear[quarterSelected].map((course: Course) => {
+                  return (
+                    <li key={course.id}>
+                      <Button
+                        variant={"outlined"}
+                        style={{
+                          backgroundColor:
+                            courseSelected?.id === course.id
+                              ? "white"
+                              : "transparent",
+                          color:
+                            courseSelected?.id === course.id
+                              ? "black"
+                              : "white",
+                          border: "1px solid white",
+                          borderRadius: "10px",
+                          width: "100%",
+                        }}
+                        onClick={() => setCourseSelected(course)}
+                      >
+                        {course.id}
+                      </Button>
+                    </li>
+                  );
+                })}
             </ul>
-            <ul className={"flex flex-col gap-3"}>
-              {quarterSelected && coursesByQuarterYear[quarterSelected].map((course: Course) => {
-                return (
-                  <li key={course.id}>
-                    <Button
-                      variant={"outlined"}
-                      style={{
-                        backgroundColor: courseSelected?.id === course.id ? 'red' : 'white',
-                        color: courseSelected?.id === course.id ? 'white' : 'black',
-                      }}
-                      onClick={() => setCourseSelected(course)
-                      }>{course.id}</Button>
-                  </li>
-                )
-              })}
-            </ul>
-
-            <div>
-              {courseSelected && courseSelected.offered.map((offer: CourseOffer) => {
+          </div>
+          <div>
+            <Typography variant={"h6"}>Professors</Typography>
+            {courseSelected &&
+              courseSelected.offered.map((offer: CourseOffer) => {
                 if (offer.quarter === quarterSelected) {
                   return (
-                    <ul　className={"flex flex-col gap-3"} key={offer.quarter}>
-                      {offer.professors.map((professor: string, index: number) => {
-                        return (
-                          <li key={`${professor}-${index}-${offer.quarter}`}>
-                            <Button
-                              variant={"outlined"}
-                              style={{
-                                backgroundColor: professorSelected === professor ? 'blue' : 'white',
-                                color: professorSelected === professor ? 'white' : 'black',
-                              }}
-                              onClick={() => setProfessorSelected(professor)
-                              }>{professor}</Button>
-                          </li>
-                        )
-                      })}
+                    <ul className={"flex flex-col gap-3"} key={offer.quarter}>
+                      {offer.professors.map(
+                        (professor: string, index: number) => {
+                          return (
+                            <li key={`${professor}-${index}-${offer.quarter}`}>
+                              <Button
+                                variant={"outlined"}
+                                style={{
+                                  backgroundColor:
+                                    professorSelected === professor
+                                      ? "white"
+                                      : "transparent",
+                                  color:
+                                    professorSelected === professor
+                                      ? "black"
+                                      : "white",
+                                  border: "1px solid white",
+                                  borderRadius: "10px",
+                                  width: "100%",
+                                }}
+                                onClick={() => setProfessorSelected(professor)}
+                              >
+                                {professor}
+                              </Button>
+                            </li>
+                          );
+                        },
+                      )}
                     </ul>
-                  )
+                  );
                 }
               })}
-            </div>
           </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
