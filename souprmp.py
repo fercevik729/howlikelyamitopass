46559a9784a49801fd0494fd0fc30489708622bd
Denny Au <legacyhyper@eduroam-169-233-225-1.ucsc.edu>
2024-01-20 13:02:39 -0800

    soup updated

--- souprmp.py
+++ souprmp.py
@@ -1,8 +1,26 @@
-'''import requests
+import requests
 from bs4 import BeautifulSoup
 
+#FIX ME
+def search_prof(query):
+	# Send a GET request to the search URL
+	page = requests.get("https://www.ratemyprofessors.com/search/professors/1078?q=" + str(query))
+	if page.status_code == 200:
+        	# Parse the HTML content
+		soup = BeautifulSoup(page.text, "html.parser")
+		print(soup.text)
+	else:
+		print("Professor not found.")	
+
+search_prof("Darrell Long")
+
+
+
+
+# REFERENCE
+'''
 # Professor URL (Long)
-url = "https://www.ratemyprofessors.com/professor/1883634"
+url = "https://www.ratemyprofessors.com/professor/304658"
 page = requests.get(url)
 
 # Selecting Page
@@ -16,7 +34,7 @@ for mytag in proftags:
 		my_dict[mytag.text] = 1
 	else:
 		my_dict[mytag.text] += 1
-# print("Tags:", my_dict) 
+print("Tags:", my_dict) 
 
 # Overall Rating
 overall = soup.find("div", {"class": "RatingValue__Numerator-qw8sqy-2 liyUjw" }).get_text(strip=True)
@@ -28,31 +46,12 @@ feedback = soup.find_all("div", {"class": "FeedbackItem__StyledFeedbackItem-uof3
 for i in feedback:
     num = i.find("div", {"class": "FeedbackItem__FeedbackNumber-uof32n-1 kkESWs"}).get_text(strip=True)
     desc = i.find("div", {"class": "FeedbackItem__FeedbackDescription-uof32n-2 hddnCs"}).get_text(strip=True)
-    # print(f"{desc}: {num}")
+    print(f"{desc}: {num}")
+
+# Comments
+try:
+	comment = soup.find("div", {"class": "Comments__StyledComments-dzzyvm-0 gRjWel"}).get_text(strip=True)
+	print("Comment:", comment)
+except:
+	print("No Comment")
 '''
-
-import requests
-from bs4 import BeautifulSoup
-import ratemyprofessor
-from RateMyProfessorPyAPI import RMPClass
-
-aapi = RMPClass.RateMyProfAPI(schoolId = 1078, teacher = "Kerry Veenstra")
-aapi.retrieveRMPInfo()
-aapi.getTags()
-professor = ratemyprofessor.get_professor_by_school_and_name(ratemyprofessor.get_school_by_name("University of California Santa Cruz"), "Kerry Veenstra")
-if professor is not None:
-    url = professor.__dict__.get('url')
-    if url is not None:
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        tags = soup.select('.Tag-bs9vf4-0')
-        for tag in tags:
-            print(tag.text)
-    else:
-        print("The URL attribute of the professor object is None.")
-        print("%s works in the %s Department of %s." % (professor.name, professor.department, professor.school.name))
-        print("Rating: %s / 5.0" % professor.rating)
-        print("Difficulty: %s / 5.0" % professor.difficulty)
-        print("Total Ratings: %s" % professor.num_ratings)
-        if professor.would_take_again is not None:
-            print(("Would Take Again: %s" % round(professor.would_take_again, 1)) + '%')
