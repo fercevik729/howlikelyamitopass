d494d73bac8fd5d795ea8d7e056f8807bdcd0e05
denny <denny@Kitsune>
2024-01-20 00:13:57 -0800

    finish the rest of the scrapping, only need professor urls

--- souprmp.py
+++ souprmp.py
@@ -1,21 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Professor URL (Long)
 url = "https://www.ratemyprofessors.com/professor/304658"
-
 page = requests.get(url)
 
+# Selecting Page
 soup = BeautifulSoup(page.text, "html.parser")
 
+# Tags
 proftags = soup.findAll("span", {"class": "Tag-bs9vf4-0 hHOVKF" })
-
 my_dict = {}
-
 for mytag in proftags:
 	if mytag.text not in my_dict:
 		my_dict[mytag.text] = 1
 	else:
 		my_dict[mytag.text] += 1
+#print("Tags:" my_dict) 
 
-print(my_dict)
-	
+# Overall Rating
+overall = soup.find("div", {"class": "RatingValue__Numerator-qw8sqy-2 liyUjw" }).get_text(strip=True)
+#print("Overall:", overall)
+
+# Feedback (Dealing with same div, would take again and difficulty)
+feedback = soup.find_all("div", {"class": "FeedbackItem__StyledFeedbackItem-uof32n-0 dTFbKx"})
+
+for i in feedback:
+    num = i.find("div", {"class": "FeedbackItem__FeedbackNumber-uof32n-1 kkESWs"}).get_text(strip=True)
+    desc = i.find("div", {"class": "FeedbackItem__FeedbackDescription-uof32n-2 hddnCs"}).get_text(strip=True)
+    # print(f"{desc}: {num}")
