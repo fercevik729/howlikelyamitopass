54fab8a331a982a79583a40371e47dc6a3aebc6e
Furkan Ercevik <ercevik.furkan@gmail.com>
2024-01-20 15:35:35 -0800

    Made course POST route transactional

--- src/app/api/course/route.ts
+++ src/app/api/course/route.ts
@@ -4,44 +4,48 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   const { title, description, units, offered } = await req.json();
 
-  let offeredIds: number[] = [];
-  for (const o of offered) {
-    const { id } = await prisma.quarterOffered.create({
-      data: {
-        quarter: o.quarter,
-        professors: {
-          connectOrCreate: o.professors.map((p: string) => {
-            return {
-              where: {
-                name: p,
-              },
-              create: {
-                name: p,
-              },
-            };
-          }),
+  const result = await prisma.$transaction(async (tx) => {
+    let offeredIds: number[] = [];
+    for (const o of offered) {
+      const { id } = await tx.quarterOffered.create({
+        data: {
+          quarter: o.quarter,
+          professors: {
+            connectOrCreate: o.professors.map((p: string) => {
+              return {
+                where: {
+                  name: p,
+                },
+                create: {
+                  name: p,
+                },
+              };
+            }),
+          },
         },
+      });
+
+      offeredIds = [...offeredIds, id];
+    }
+
+    const { id } = await tx.course.create({
+      data: {
+        title,
+        description,
+        units: parseInt(units.trim()),
+        offered: {
+          connect: offeredIds.map((id) => ({ id })),
+        },
+      },
+      select: {
+        id: true,
       },
     });
 
-    offeredIds = [...offeredIds, id];
-  }
-
-  const { id } = await prisma.course.create({
-    data: {
-      title,
-      description,
-      units: parseInt(units.trim()),
-      offered: {
-        connect: offeredIds.map((id) => ({ id })),
-      },
-    },
-    select: {
-      id: true,
-    },
+    return { courseId: id };
   });
 
-  return NextResponse.json({ courseId: id });
+  return NextResponse.json(result);
 }
 
 export async function GET(req: NextRequest) {
