b948c28ef74d15d5a45a296c43aa526623d7b86e
Jose Chavez <josmchavz@gmail.com>
2024-01-20 21:08:59 -0800

    added stats, not reviews

--- src/components/ProfessorStats.tsx
+++ src/components/ProfessorStats.tsx
@@ -1,7 +1,56 @@
-export default function ProfessorStats() {
+import { Professor } from "@/types/model";
+import { Typography } from "@mui/material";
+
+interface ProfessorStatsProps {
+  professor: Professor;
+}
+
+export default function ProfessorStats({ professor }: ProfessorStatsProps) {
   return (
-    <div>
-      <h1>Professor Stats</h1>
+    <div className="border border-white w-[450px] py-[35px] px-[40px]">
+      <div className="rating flex justify-center font-mono">
+        <span className={"text-[85px]"}>{professor.rating}</span>
+        <span className={"text-[40px] mt-[18px] text-[#E3D9FF]"}>/5</span>
+      </div>
+      <Typography
+        variant={"h3"}
+        className={"text-3xl text-center font-mono mb-6"}
+      >
+        {professor.name}
+      </Typography>
+      <div id="stats" className={"flex justify-between"}>
+        <div
+          id="difficulty"
+          className={"flex flex-col justify-center font-mono"}
+        >
+          <span className={"text-4xl text-center"}>{professor.difficulty}</span>
+          <Typography className={"text-sm font-thin"}>
+            level of difficulty
+          </Typography>
+        </div>
+        <div id="again" className={"flex flex-col justify-center font-mono"}>
+          <span className={"text-4xl text-center"}>
+            {professor.wouldRepeat}
+          </span>
+          <Typography className={"text-sm font-thin"}>
+            would take it again
+          </Typography>
+        </div>
+      </div>
+      <div id="tags">
+        <div className={"flex flex-wrap gap-2 mt-5 font-mono"}>
+          {professor.tags.map((tag, index) => (
+            <span
+              key={index}
+              className={
+                "text-sm font-thin px-3 py-1 border border-white rounded-xl"
+              }
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
