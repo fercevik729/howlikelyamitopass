99c64476ceee4589aa915512981752d20cfab564
denny <denny@Kitsune>
2024-01-21 03:47:24 -0800

    adding pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,20 +1,44 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Results() {
+  // Preventing about me to have the ability to scroll
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    document.body.style.height = "100vh";
+    return () => {
+      document.body.style.overflow = "";
+      document.body.style.height = "";
+    };
+  }, []);
+
   return (
-    <main>
-<<<<<<< HEAD
-      <section id={"hero"} className={`flex flex-col py-36 items-center`}>
-	<h1 className="text-2xl mb-8 text-center"> About</h1>
-        <p className="text-2xl mb-8 text-center">A seamless interaction from RateMyProfessor's data to AI that provides summarized information of the professor's teaching environment. Our platform also provides the UCSC course catalog as well. Our goal is inform computer science undergraduates of their chances in passing a course and providing the information they need to succeed. </p>
-=======
-      <section id={"about_us"} className={"flex py36 items-center"}>
-        <div className={"flex flex-col gap-[52px] text-white"}>
-          <h1 className={"text-6x1 font-bold"}> About</h1>
-        </div>
->>>>>>> 4eed83c7ae82f23f8cedb6ab2fca42f56ab59b0d
-      </section>
-    </main>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        marginTop: "20vh",
+        height: "50vh",
+      }}
+    >
+      <h1 style={{ fontSize: "8rem", fontWeight: "bold", color: "#fff" }}>
+        About
+      </h1>
+      <p
+        style={{
+          color: "#cff",
+          fontSize: "1.25rem",
+          maxWidth: "1200px",
+          textAlign: "center",
+        }}
+      >
+        A seamless interaction from RateMyProfessor&apos;s data to AI that
+        provides summarized information of the professor&apos;s teaching
+        environment. Our platform also provides the UCSC course catalog as well.
+        Our goal is to inform computer science undergraduates of their chances
+        in passing a course and providing the information they need to succeed.
+      </p>
+    </div>
   );
 }
