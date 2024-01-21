c65ca4d63bbc56ddb033b2b2d622016a7bbd9273
Daniel Chang <dawichan@ucsc.edu>
2024-01-20 21:47:06 -0800

    Added ResultsPercentage component

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import React from "react";
+import ResultsPercentage from "@/components/ResultsPercentage";
 
 export default function Results() {
   return (
     <main>
       <section id={"hero"} className={`flex flex-col py-36 items-center`}>
         <p className="text-2xl mb-8 text-center">hi</p>
+        <ResultsPercentage percentage={93} />
       </section>
     </main>
   );
