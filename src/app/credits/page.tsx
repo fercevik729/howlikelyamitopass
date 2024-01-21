05fd9d664e010630f6b16bba3bc68e073ab416a2
denny <denny@Kitsune>
2024-01-21 01:33:02 -0800

    barebones credit

--- src/app/credits/page.tsx
+++ src/app/credits/page.tsx
@@ -1,1 +1,13 @@
-export default function Credits() {}
+"use client";
+import React from "react";
+
+export default function Results() {
+  return (
+    <main>
+      <section id={"hero"} className={`flex flex-col py-36 items-center`}>
+	<h1 className="text-2xl mb-8 text-center">Credits</h1>
+	<p className="text-2xl mb-8 text-center">RateMyProfessorAPI</p>
+      </section>
+    </main>
+  );
+}
