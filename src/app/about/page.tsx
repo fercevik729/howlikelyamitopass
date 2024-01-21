ec3d14f19687de6d345e51162fdf26a33e4650c1
denny <denny@Kitsune>
2024-01-21 01:28:12 -0800

    bare bones about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,13 @@
-export default function About() {
+"use client";
+import React from "react";
+
+export default function Results() {
   return (
-    <>
-      <main>
-        <section id={"about_us"} className={"flex py36 items-center"}>
-          <div className={"flex flex-col gap-[52px] text-white"}>
-            <h1 className={"text-6x1 font-bold"}> About</h1>
-          </div>
-        </section>
-      </main>
-    </>
+    <main>
+      <section id={"hero"} className={`flex flex-col py-36 items-center`}>
+	<h1 className="text-2xl mb-8 text-center"> About</h1>
+        <p className="text-2xl mb-8 text-center">A seamless interaction from RateMyProfessor's data to AI that provides summarized information of the professor's teaching environment. Our platform also provides the UCSC course catalog as well. Our goal is inform computer science undergraduates of their chances in passing a course and providing the information they need to succeed. </p>
+      </section>
+    </main>
   );
 }
